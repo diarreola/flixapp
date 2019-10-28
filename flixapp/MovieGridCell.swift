3fdb53f8bb4f7e52e464a6c862c3383a565dbf1f
Diana Arreola <arreoladiana11@gmail.com>
2019-10-27 21:02:53 -0700

    superhero image view

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by smoltamal on 10/27/19.
+//  Copyright © 2019 diarreola. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
